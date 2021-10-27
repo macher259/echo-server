a6a556ea6110323cddfaf638e9e2912d3569c765
Maciej Herdon <mh418267@students.mimuw.edu.pl>
2021-10-27 09:39:03 +0200

    Echo Server initial implementation.

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+use std::net::SocketAddr;
+use tokio::io;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+const BUFFER_SIZE: usize = 1024;
+
+pub struct ServerTCP {
+    listener: TcpListener,
+}
+
+impl ServerTCP {
+    pub async fn new(mut address: String, port: u16) -> io::Result<ServerTCP> {
+        let port = port.to_string();
+        address.push(':');
+        address.push_str(port.as_str());
+
+        let listener = TcpListener::bind(&address).await?;
+
+        println!("Started listening on {}.", address);
+
+        Ok(ServerTCP { listener })
+    }
+
+    pub async fn run(&self) {
+        loop {
+            let accept = self.listener.accept().await;
+
+            if accept.is_err() {
+                eprintln!("Error on accept: {}", accept.unwrap_err());
+                continue;
+            }
+
+            let (socket, address) = accept.unwrap();
+
+            tokio::spawn(async move {
+                process_socket(socket, address).await;
+            });
+        }
+    }
+}
+
+async fn process_socket(mut socket: TcpStream, address: SocketAddr) {
+    println!("{}: Connected.", address);
+
+    let mut buffer: Vec<u8> = vec![0; BUFFER_SIZE];
+
+    loop {
+        match socket.read(&mut buffer).await {
+            Ok(0) => {
+                println!("{}: Connection with the socket closed successfully.", address);
+                return;
+            }
+            Ok(n) => {
+                let tmp = socket.write_all(&buffer[0..n]).await;
+                if tmp.is_err() {
+                    eprintln!("{}: Error while writing to the socket: {}.", address, tmp.unwrap_err());
+                    return;
+                }
+            }
+            Err(e) => {
+                eprintln!("{}: Error while reading the socket: {}.", address, e);
+                return;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::process;
+use echo_server::ServerTCP;
+
+const HOSTNAME: &str = "localhost";
+const PORT: u16 = 2137;
+
+#[tokio::main]
+async fn main() {
+    let server = ServerTCP::new(HOSTNAME.to_string(), PORT).await.unwrap_or_else(|e| {
+        eprintln!("Couldn't connect: {}.", e);
+        process::exit(1);
+    });
+
+    server.run().await;
 }
